ac538e55e6458dd4b294b4ce5d66f426982765ac
João Arruda <joaoarruda.info@gmail.com>
2020-03-27 21:40:06 -0300

    created tests env and tdd practices;

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 const express = require('express'); 
 const app = express(); 
 const cors = require('cors');
-const port = 3333; 
 const routes = require('./routes');
+
+const { errors } = require('celebrate');
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
-app.listen(port); 
+module.exports = app;  
 
 
 /**
--- src/controllers/OngController.js
+++ src/controllers/OngController.js
@@ -1,6 +1,5 @@
-const crypto = require('crypto');
 const connection = require('../database/connection')
-
+const generateUniqueId = require('../utils/generateUniqueId');
 module.exports = {
     async index(req, res){
         const ongs = await connection('ong').select('*');
@@ -10,7 +9,7 @@ module.exports = {
 
     async create(req, res) {
         const { name, email, whatsapp, city, uf } = req.body;
-        const id = crypto.randomBytes(4).toString('HEX');
+        const id = generateUniqueId();
 
         await connection('ong').insert({
             id, name, email, whatsapp, city, uf
